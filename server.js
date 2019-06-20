2be1126f076e686ee9253dfee989f170b9a291be
Sansevieria <elizabethleighmather@gmail.com>
2019-06-20 16:39:16 -0500

    external js moved to html

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var htmlRoutes = require('./app/routing/htmlRoutes.js');
 
 // express
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8000;
 
 // static
 app.use(express.static(path.join(__dirname, './app/public')));
